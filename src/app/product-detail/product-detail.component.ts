b977c65399afae9712aecf37a98a598adcaf484a
jairoDGM <jairo.danielgm@gmail.com>
2024-05-16 19:35:22 -0600

    implementacion vista detalle productos v1

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product, productList } from '../products/products.mock';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
+  product?: Product;
+  productList: Product[] = productList;
+  loading: boolean = true;
+  color: string = "";
+
+  constructor(private _route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this._route.params.subscribe(params => {
+        console.log(params['productId']);
+        this.product = this.productList.find(product => product.id == params['productId']);
+        this.color = this.product?.price as number > 10 ? 'red' : '';
+        this.loading = false
+      });
+    }, 700);
+
+  }
 
 }
